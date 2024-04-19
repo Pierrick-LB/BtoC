5cccbc8dd9e4d859a345541ae8dea109af56a00f
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-04-19 15:37:05 +0200

    fixed shop template

--- B2C/urls.py
+++ B2C/urls.py
@@ -1,9 +1,28 @@
+"""
+URL configuration for B2C project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.urls import path
 from .views import homePage, contactPage
+from .views import shopPage, shopPage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", homePage, name="homePage"),
     path("contact/", contactPage, name="contactPage"),
-    path("shop/", shopPage, name="shopPage"),
+    path("shop/", shopPage, name="shopPage")
 ]
+
