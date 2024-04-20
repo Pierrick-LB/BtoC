788f7dec72008a37fc519e083038343441f09738
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-04-20 16:29:15 +0200

    shop template

--- B2C/urls.py
+++ B2C/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import homePage, contactPage
-from .views import shopPage, shopPage
+from Products.views import shopPage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
