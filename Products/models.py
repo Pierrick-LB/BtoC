d121b77a8a72e24f301a464ada43b76e0a33e597
Pierrick-LB <pierrick.lb2003@gmail.com>
2024-04-29 15:59:25 +0200

    add article table

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    numero_article = models.IntegerField()
+    nom = models.CharField(max_length=100)
+    prix = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    date = models.DateField()
+    categorie = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    image = models.URLField()
