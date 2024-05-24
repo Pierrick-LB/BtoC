8b9955d8cba398c5c2f4edc657a97ccce0b5a561
Awaa013 <waw.diallo.edu@groupe-gema.com>
2024-05-24 16:43:33 +0200

    PotsgreSQL database

--- Comments/models.py
+++ Comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 
+
 class Comment(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
